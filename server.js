2dea6c5280a5d9552f05fe2895a247d1a3dce6a6
cryptokiddd <avaliani1232@gmail.com>
2025-11-03 14:31:14 +0400

    s

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
 // Body parser
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
